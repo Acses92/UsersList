b66df367c3f8fd24e8da332855ec05745e5e36e4
Acses92 <40656486+Acses92@users.noreply.github.com>
2022-05-14 20:28:16 +0500

    Add call and email intents. Trouble with data format

--- app/src/main/java/com/anatolykravchenko/waveaccesstest/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/anatolykravchenko/waveaccesstest/presentation/detail/DetailFragment.kt
@@ -1,15 +1,19 @@
 package com.anatolykravchenko.waveaccesstest.presentation.detail
 
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import com.anatolykravchenko.waveaccesstest.R
 import com.anatolykravchenko.waveaccesstest.databinding.DetailFragmentBinding
 import com.anatolykravchenko.waveaccesstest.data.model.UserItemUi
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 class DetailFragment: Fragment(R.layout.detail_fragment) {
@@ -22,7 +26,9 @@ class DetailFragment: Fragment(R.layout.detail_fragment) {
         setupUi()
         backButtonPress()
         emailClickListener()
+        phoneClickListener()
     }
+
 
     private fun setupUi() {
         binding.userNameDetailValueTv.text = user?.name
@@ -33,24 +39,74 @@ class DetailFragment: Fragment(R.layout.detail_fragment) {
         binding.userAddressDetailValueTv.text = user?.address
         binding.userAboutDetailValueTv.text = user?.about
         binding.userRegisteredDetailValueTv.text = user?.registered
+      //  dataFormatter()
+        eyeColorImageSetup()
+        favoriteFruitImageSetup()
+    }
+
+    private fun dataFormatter() {
+        val formatter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss SSSXXX" )
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        val data = user?.registered
+        binding.userRegisteredDetailValueTv.text =
+                LocalDate.parse(data, formatter).toString()
+    }
+
+    private fun eyeColorImageSetup() {
         val eyeColor= user?.eyeColor
         if (eyeColor != null) {
             when {
-                eyeColor.contains("green") -> binding.eyeColorImangeView.setImageResource(R.drawable.green_eye_round)
-                eyeColor.contains("blue") ->binding.eyeColorImangeView.setImageResource(R.drawable.blue_eye_round)
-                eyeColor.contains("brown") -> binding.eyeColorImangeView.setImageResource(R.drawable.brown_eye_round)
+                eyeColor.contains("green") ->
+                    binding.eyeColorImangeView.setImageResource(R.drawable.green_eye_round)
+                eyeColor.contains("blue") ->
+                    binding.eyeColorImangeView.setImageResource(R.drawable.blue_eye_round)
+                eyeColor.contains("brown") ->
+                    binding.eyeColorImangeView.setImageResource(R.drawable.brown_eye_round)
                 else ->binding.eyeColorImangeView.setImageLevel(R.drawable.brown_eye_round)
             }
         }
     }
 
-    private fun emailClickListener() {
-        binding.userEmailDetailValueTv.setOnClickListener {
-            Toast.makeText(context, "Тык", Toast.LENGTH_LONG)
-                .show()
+    private fun favoriteFruitImageSetup() {
+        val favoriteFruit = user?.favoriteFruit
+        if(favoriteFruit != null) {
+            when{
+                favoriteFruit.contains("banana") ->
+                    binding.favoriteFruiteImageView.setImageResource(R.drawable.ic_banana_draw)
+                favoriteFruit.contains("strawberry") ->
+                    binding.favoriteFruiteImageView.setImageResource(R.drawable.ic_strawberry_draw)
+                favoriteFruit.contains("apple") ->
+                    binding.favoriteFruiteImageView.setImageResource(R.drawable.ic_apple_draw)
+            }
         }
     }
 
+
+
+    private fun emailClickListener() {
+        binding.userEmailDetailValueTv.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, user?.email)
+                putExtra(Intent.EXTRA_SUBJECT, "Hello")
+            }
+            //добавить проверку есть ли отправка сообщений
+            startActivity(intent)
+        }
+    }
+
+    private fun phoneClickListener() {
+        binding.userPhoneDetailValueTv.setOnClickListener {
+            //добавить проверку, есть ли доступ к телефону
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:"+ user?.phone)
+            startActivity(intent)
+        }
+
+    }
 
 
     private fun backButtonPress() {
