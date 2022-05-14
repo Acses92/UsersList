e0da80989fc3de85d6ed8693561c8f37e55cbb91
Acses92 <40656486+Acses92@users.noreply.github.com>
2022-05-14 15:38:04 +0500

    UserListAdapter and UserListFragment fix

--- app/src/main/java/com/anatolykravchenko/waveaccesstest/presentation/adapters/UserListAdapter.kt
+++ app/src/main/java/com/anatolykravchenko/waveaccesstest/presentation/adapters/UserListAdapter.kt
@@ -3,14 +3,22 @@ package com.anatolykravchenko.waveaccesstest.presentation.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.anatolykravchenko.waveaccesstest.data.model.UserItemUi
 import com.anatolykravchenko.waveaccesstest.databinding.UserItemBinding
 
 class UserListAdapter(private val onItemClicked: (UserItemUi) ->Unit):
-    RecyclerView.Adapter<UserViewHolder>() {
+    ListAdapter<UserItemUi, UserViewHolder>(
+        object: DiffUtil.ItemCallback<UserItemUi>() {
+            override fun areContentsTheSame(oldItem: UserItemUi, newItem: UserItemUi): Boolean =
+                    oldItem == newItem
 
-    val myData = mutableListOf<UserItemUi>()
+            override fun areItemsTheSame(oldItem: UserItemUi, newItem: UserItemUi): Boolean =
+                    oldItem.name ==newItem.name
+        }
+    ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         return UserViewHolder(
@@ -23,18 +31,16 @@ class UserListAdapter(private val onItemClicked: (UserItemUi) ->Unit):
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val user = myData[position]
-        holder.apply {
-            binding.userNameViewHolder.text = user.name
-            binding.userNameViewHolder.text = user.email
-            if(user.isActive) {
-                binding.isActiveStatusViewHolder.isVisible
-                holder.itemView.setOnClickListener{onItemClicked(user)}
+        with(holder.binding) {
+            val item = getItem(position)
+            userNameViewHolder.text = item.name
+            userEmailViewHolder.text = item.email
+            if(item.isActive) {
+                isActiveStatusViewHolder.isVisible = true
+                root.setOnClickListener { onItemClicked(item) }
             }
         }
     }
 
-    override fun getItemCount(): Int = myData.size
 
-    fun addUser(list: List<UserItemUi>) = myData.addAll(list)
 }
--- app/src/main/java/com/anatolykravchenko/waveaccesstest/presentation/list/UserListFragment.kt
+++ app/src/main/java/com/anatolykravchenko/waveaccesstest/presentation/list/UserListFragment.kt
@@ -2,7 +2,8 @@ package com.anatolykravchenko.waveaccesstest.presentation.list
 
 import android.os.Bundle
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,12 +12,15 @@ import dagger.hilt.android.AndroidEntryPoint
 import com.anatolykravchenko.waveaccesstest.R
 import com.anatolykravchenko.waveaccesstest.databinding.UserListFragmentBinding
 import com.anatolykravchenko.waveaccesstest.presentation.adapters.UserListAdapter
+import com.anatolykravchenko.waveaccesstest.presentation.list.UserState.Success
+import com.anatolykravchenko.waveaccesstest.presentation.list.UserState.Error
+import com.anatolykravchenko.waveaccesstest.presentation.list.UserState.Loading
+
 
 @AndroidEntryPoint
 class UserListFragment: Fragment(R.layout.user_list_fragment) {
     private val binding by viewBinding(UserListFragmentBinding::bind)
     private val viewModel by viewModels<UserListViewModel>()
-    private lateinit var adapter: UserListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,6 +28,26 @@ class UserListFragment: Fragment(R.layout.user_list_fragment) {
         with(binding.usersList) {
             adapter = userListAdapter
             layoutManager = LinearLayoutManager(context)
+        }
+
+        viewModel.screenState.observe(viewLifecycleOwner) {state: UserState? ->
+            when(state) {
+                is Error -> {
+                    binding.usersList.isVisible = false
+                    Toast.makeText(context, "Ошибка", Toast.LENGTH_LONG)
+                        .show()
+                }
+                is Success ->{
+                    binding.usersList.isVisible = true
+                    userListAdapter.submitList(state.users)
+                    Toast.makeText(context, "Всё ок", Toast.LENGTH_LONG)
+                        .show()
+                }
+                is Loading -> {
+                    binding.usersList.isVisible = false
+                }
+            }
+
         }
     }
 }
