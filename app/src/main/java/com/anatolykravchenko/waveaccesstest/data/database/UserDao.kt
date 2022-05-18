f2316234f959105b1ad725af06680da4329fa43e
Acses92 <40656486+Acses92@users.noreply.github.com>
2022-05-18 11:34:38 +0500

    Add onConflict strategy.

--- app/src/main/java/com/anatolykravchenko/waveaccesstest/data/database/UserDao.kt
+++ app/src/main/java/com/anatolykravchenko/waveaccesstest/data/database/UserDao.kt
@@ -2,6 +2,7 @@ package com.anatolykravchenko.waveaccesstest.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.anatolykravchenko.waveaccesstest.data.database.entity.UserItemEntity
 
@@ -22,7 +23,7 @@ interface UserDao {
     /**
      * Добавляет список пользователей
      */
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(userItemEntity: List<UserItemEntity>)
 
 }
